b3ef06d90acb0bcaa56b9f197031dc93a021a77e
fwnnio0 <1725110187@utectulancingo.edu.mx>
2026-05-15 23:08:45 +0000

    CREATED clase 2

---
+++ ejercicios/semana2/clase_smartphone.py
@@ -0,0 +1,29 @@
+class Smartphone:
+    def __init__(self, marca, color, numero_telefono, modelo, sistema_operativo, 
+                capacidad_almacenamiento, memoria_ram, esta_encendido,
+                esta_bloqueado, porcentaje_bateria):
+        
+        self.marca = marca
+        self.color = color
+        self.numero_telefono = numero_telefono
+        self.modelo = modelo
+        self.sistema_operativo = sistema_operativo
+        self.capacidad_almacenamiento = capacidad_almacenamiento
+        self.memoria_ram = memoria_ram
+        self.esta_encendido = esta_encendido
+        self.esta_bloqueado = esta_bloqueado
+        self.porcentaje_bateria = porcentaje_bateria
+
+        print(f"Marca del teléfono: {self.marca}")
+        print(f"Color: {self.color}")
+        print(f"Número de teléfono: {self.numero_telefono}")
+        print(f"Módelo: {self.modelo}")
+        print(f"Sistema Operativo: {self.sistema_operativo}")
+        print(f"Capacidad de alamacenamiento: {self.capacidad_almacenamiento}")
+        print(f"Memoria Ram: {self.memoria_ram}")
+        print(f"¿Está encendido?: {self.esta_encendido}")
+        print(f"¿Está bloqueado?: {self.esta_bloqueado}")
+        print(f"Porcentaje de la batería: {self.porcentaje_bateria}")
+
+s26ultra = Smartphone("Samsung", "Verde limón", +527751897762, "Samsung Galaxy S26 Ultra",
+                      "Android 16", 256, 16, True, False, 80)
