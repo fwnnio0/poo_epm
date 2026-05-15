09717e16b8a890634cf9b23b04b19d660d875880
fwnnio0 <1725110187@utectulancingo.edu.mx>
2026-05-15 23:48:45 +0000

    CREATED clase 3

---
+++ ejercicios/semana2/clase_personaje_rpg.py
@@ -0,0 +1,55 @@
+class PersonajeRPG:
+    def __init__(self, nombre, vida, nivel, fuerza, velocidad, inteligencia,
+                 tipo_arma, defensa, expericencia, energia):
+        
+        self.nombre = nombre
+        self.vida = vida
+        self.nivel = nivel
+        self.fuerza = fuerza
+        self.velocidad = velocidad
+        self.inteligencia = inteligencia
+        self.tipo_arma = tipo_arma
+        self.defensa = defensa
+        self.experiencia = expericencia
+        self.energia = energia
+
+    def mostrarDatos(self):
+        print(f"Nombre: {self.nombre}")
+        print(f"Vida: {self.vida}")
+        print(f"Nivel: {self.nivel}")
+        print(f"Fuerza: {self.fuerza}")
+        print(f"Velocidad: {self.velocidad}")
+        print(f"Inteligencia: {self.inteligencia}")
+        print(f"Tipo de arma: {self.tipo_arma}")
+        print(f"Defensa: {self.defensa}")
+        print(f"Expericiencia: {self.experiencia}")
+        print(f"Energía: {self.energia}")
+    
+    def usarHabilidad(self, habilidad):
+        print("Su habilidad: ", habilidad)
+
+    def completarMision(self, mision):
+        print("Su misión: ", mision)
+
+    def equiparObjeto(self, objeto):
+        print("Su objeto: ", objeto)
+
+    def atacarEnemigo(self, enemigo):
+        print("Su enemigo: ", enemigo)
+
+    def decirFrase(self, frase):
+        print("Su frase: ", frase)
+
+    def morir(self):
+        print("Derrotado")
+
+gwen = PersonajeRPG("Gwen", 620, 1, 75, 85, 90, "Tijeras Mágicas",
+                    40, 0, 100)
+
+gwen.mostrarDatos()
+gwen.usarHabilidad("Corte Celestial")
+gwen.completarMision("Defender el Nexo")
+gwen.equiparObjeto("Ore espiritual")
+gwen.atacarEnemigo("Yasuo")
+gwen.decirFrase("El viento lo elige todo")
+gwen.morir
